0acce7d4a154a66fc6285d5073cc8ee161591ff9
Dan Dresser <ddresser05@gmail.com>
2025-01-27 06:44:31 -0600

    Added a field to preserve order in values inserted into DataSeries; last_counter_id keeps track of how many values inserted in DataSeries so the hash is always unique, even if deleting a value of a certain type and then later inserting a value of that same type again

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::any::{Any, TypeId};
 use std::collections::HashMap;
 use std::hash::{Hash, Hasher};
-use std::fmt::{self, Debug};
+use std::fmt::Debug;
 
 trait NamedAny: Any + Debug {
     fn type_name(&self) -> &'static str;
@@ -23,49 +23,56 @@ impl<T: Any + Debug> NamedAny for T {
     }
 }
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 struct TypeKey {
     type_id: TypeId,
-    name: String,
+    counter_id: usize,
 }
 
 impl TypeKey {
-    fn new(type_id: TypeId, name: String) -> TypeKey {
-        TypeKey { type_id, name }
+    fn new(type_id: TypeId, counter_id: usize) -> TypeKey {
+        TypeKey { type_id, counter_id }
     }
 }
 
 impl Hash for TypeKey {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.type_id.hash(state);
-        self.name.hash(state);
+        self.counter_id.hash(state);
     }
 }
 
 #[derive(Debug)]
 struct DataSeries {
     data: HashMap<TypeKey, Box<dyn NamedAny>>,
+    order: Vec<TypeKey>,
+    last_counter_id: usize,
 }
 
 impl DataSeries {
     fn new() -> DataSeries {
         DataSeries {
             data: HashMap::new(),
+            order: Vec::new(),
+            last_counter_id: 0,
         }
     }
 
     fn push<T: 'static + Debug>(&mut self, value: T) {
         let key = TypeKey::new(
             TypeId::of::<T>(),
-            String::from(format!("col_{}", self.data.len())),
+            self.last_counter_id,
         );
+        self.order.push(key);
         self.data.insert(key, Box::new(value));
+        self.last_counter_id += 1;
     }
 
     fn print_all(&self) {
-        for (key, value) in &self.data {
-            println!("Column: {}", key.name);
-            value.print_value();
+        for key in &self.order {
+            if let Some(value) = self.data.get(key) {
+                value.print_value();
+            }
         }
     }
 }
